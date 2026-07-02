57ee58658aa72899040093b807c6237dc1284acc
quartascode <brenoposavac@proton.me>
2026-07-02 19:30:47 -0300

    update README.md

--- README.md
+++ README.md
@@ -6,15 +6,17 @@ Raytracer simples feito a fim de estudos
 - Intersecção de esferas
 - Refração e reflexão total em dielétricos
 - Movimentação da câmera e ponto o qual ela mira
+- Fov vertical variável
 
 ## Exemplos
+> **Obs.:** todos os exemplos são renderizados em 1080p com 1000 samples por pixel e um limite de 50 bounces
 ![Imagem um](examples/image.jpg)
 
-![Imagem dois](examples/render.jpg)
+![Imagem dois](examples/image1.jpg)
+
+![Imagem três](examples/render.jpg)
 
 ## Como compilar
 ```sh
 make raytracer && ./raytracer > image.ppm
 ```
-
-Pode mudar "image" para qualquer outro nome, mas deve ser .ppm
--- src/main.c
+++ src/main.c
@@ -15,57 +15,62 @@ int main() {
 	Camera camera;
 
 	camera.aspectRatio = 16.0/9;
-	camera.imageWidth = 400;
-	camera.samplesPerPixel = 100;
-	camera.rayBounceLimit = 10;
+	camera.imageWidth = 1920;
+	camera.samplesPerPixel = 1000;
+	camera.rayBounceLimit = 50;
 
-	camera.vFov = 90;
+	camera.vFov = 20;
 	camera.lookAt = (Vec3){0, 0, -2.0};
-	int k = 1;
-	camera.lookFrom = (Vec3){2*k, 3*k, 1*k};
+	// 0 4 10
+	camera.lookFrom = (Vec3){0, 4, 10};
 	camera.vUp = (Vec3){0, 1, 0};
 
 	// world
-	Material metal = {
-		.type = METAL,
-		.albedo = (Color){0.8, 0.8, 0.8},
-		.fuzz = 0.0
-	};
-	Material whiteMetal = {
-		.type = METAL,
-		.albedo = (Color){1.0, 1.0, 1.0},
-		.fuzz = 0.5
-	};
-	Material grayLambertian = {
-		.type = LAMBERTIAN,
-		.albedo = (Color){0.3, 0.3, 0.3}
-	};
-	Material redLambertian = {
-		.type = LAMBERTIAN,
-		.albedo = (Color){0.7, 0.0, 0.0}
-	};
-	Material blueLambertian = {
-		.type = LAMBERTIAN,
-		.albedo = (Color){0.0, 0.3, 0.5}
-	};
-	Material Glass = {
+	Material glass = {
 		.type = DIELETRIC,
-		.albedo = (Color){1.0, 1.0, 1.0},
+		.albedo = (Color){0.99, 0.99, 0.99},
 		.refractionIndex = 1.5
 	};
 	Material insideGlass = {
 		.type = DIELETRIC,
-		.albedo = (Color){0.0, 0.5, 0.7},
+		.albedo = (Color){1.0, 1.0, 1.0},
 		.refractionIndex = 1.0 / 1.5
+	};
+	Material water = {
+		.type = DIELETRIC,
+		.albedo = (Color){1.0, 1.0, 1.0},
+		.refractionIndex = 1.33
+	};
+	Material mirror = {
+		.type = METAL,
+		.albedo = (Color){1.0, 1.0, 1.0},
+		.fuzz = 0.0
+	};
+	Material metal = {
+		.type = METAL,
+		.albedo = (Color){0.4, 0.4, 0.4},
+		.fuzz = 0.5
+	};
+	Material planet = {
+		.type = LAMBERTIAN,
+		.albedo = (Color){0.2, 0.4, 0.8}
+	};
+	Material sphereOne = {
+		.type = LAMBERTIAN,
+		.albedo = (Color){0.7, 0.7, 0.0}
+	};
+	Material sphereTwo = {
+		.type = LAMBERTIAN,
+		.albedo = (Color){0.5, 0.0, 0.5}
 	};
 
 	Sphere spheres[6] = {
-		{ .center = {0, -100.5, -2}, .radius = 100, .material = &grayLambertian },
-		{ .center = {-1.0, 1, -4}, .radius = 1.5, .material = &metal },
-		{ .center = {1.0, 0, -2}, .radius = 0.5, .material = &redLambertian },
-		{ .center = {1.2, 1.25, -0.45}, .radius = .75, .material = &Glass },
-	//	{ .center = {0.0, 0, -2}, .radius = 0.4, .material = &insideGlass },
-		{ .center = {0.0, 0, -2.5}, .radius = 0.5, .material = &blueLambertian },
+		{ .center = {0, -100.5, -2}, .radius = 100, .material = &planet },
+		{ .center = {1.0, 0, -2}, .radius = 0.5, .material = &sphereOne },
+		{ .center = {0.0, 0, -2}, .radius = 0.5, .material = &metal },
+		{ .center = {-1.0, 0, -2}, .radius = 0.5, .material = &sphereTwo },
+		{ .center = {0.5, 0.0, -1.0}, .radius = 0.5, .material = &glass },
+		{ .center = {0.5, 0.0, -1.0}, .radius = 0.4, .material = &insideGlass },
 	};
 
 	World world = {
